75cbb9e41d40f3460e4569c8bba311da210db10e
Noahl11 <noahmznri@gmail.com>
2015-12-15 15:47:38 -0500

    update

--- Minecraft/srcb/main/java/com/Noahl11/terraria/proxy/ClientProxy.java
+++ Minecraft/srcb/main/java/com/Noahl11/terraria/proxy/ClientProxy.java
@@ -1,5 +1,12 @@
 package com.Noahl11.terraria.proxy;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import net.minecraft.client.model.ModelBiped;
+import net.minecraft.item.Item;
+
+import com.Noahl11.terraria.Entity.armor.Mining_helmet;
 import com.Noahl11.terraria.init.TerrariaArmor;
 import com.Noahl11.terraria.init.TerrariaBlocks;
 import com.Noahl11.terraria.init.TerrariaItems;
@@ -7,8 +14,13 @@ import com.Noahl11.terraria.init.TerrariaPlants;
 
 public class ClientProxy extends CommonProxy {
 	
+	public static final Map<Item, ModelBiped> armorModels = new HashMap<Item, ModelBiped>();
+	
 	@Override
 	public void registerRenders() {
+		Mining_helmet Mining_Helm = new Mining_helmet();
+		armorModels.put(TerrariaArmor.Mining_Helmet, Mining_Helm);
+		
 		TerrariaItems.registerRenders();
 		TerrariaBlocks.registerRenders();
 		TerrariaPlants.registerRenders();
