fa5b36a0d000865132c7648b6fbbe0067f6b0e08
Noahl11 <noahmznri@gmail.com>
2015-12-15 15:45:31 -0500

    Armor model

---
+++ Minecraft/srcb/main/java/com/Noahl11/terraria/Entity/armor/Mining_helmet.java
@@ -0,0 +1,125 @@
+package com.Noahl11.terraria.Entity.armor;
+
+import net.minecraft.client.model.ModelBase;
+import net.minecraft.client.model.ModelBiped;
+import net.minecraft.client.model.ModelRenderer;
+import net.minecraft.entity.Entity;
+
+public class Mining_helmet extends ModelBiped
+{
+  //fields
+    ModelRenderer Helmrim;
+    ModelRenderer head;
+    ModelRenderer body;
+    ModelRenderer rightarm;
+    ModelRenderer leftarm;
+    ModelRenderer rightleg;
+    ModelRenderer leftleg;
+    ModelRenderer Helmrim1;
+    ModelRenderer Shape1;
+    ModelRenderer Shape2;
+    ModelRenderer Shape3;
+  
+  public Mining_helmet()
+  {
+    textureWidth = 64;
+    textureHeight = 64;
+    
+      Helmrim = new ModelRenderer(this, 0, 0);
+      Helmrim.addBox(-5F, -9F, -5F, 10, 2, 10);
+      Helmrim.setRotationPoint(0F, 0F, 0F);
+      Helmrim.setTextureSize(64, 32);
+      Helmrim.mirror = true;
+      setRotation(Helmrim, 0F, 0F, 0F);
+      head = new ModelRenderer(this, 0, 0);
+      head.addBox(-4F, -8F, -4F, 8, 8, 8);
+      head.setRotationPoint(0F, 0F, 0F);
+      head.setTextureSize(64, 32);
+      head.mirror = true;
+      setRotation(head, 0F, 0F, 0F);
+      body = new ModelRenderer(this, 16, 16);
+      body.addBox(-4F, 0F, -2F, 8, 12, 4);
+      body.setRotationPoint(0F, 0F, 0F);
+      body.setTextureSize(64, 32);
+      body.mirror = true;
+      setRotation(body, 0F, 0F, 0F);
+      rightarm = new ModelRenderer(this, 40, 16);
+      rightarm.addBox(-3F, -2F, -2F, 4, 12, 4);
+      rightarm.setRotationPoint(-5F, 2F, 0F);
+      rightarm.setTextureSize(64, 32);
+      rightarm.mirror = true;
+      setRotation(rightarm, 0F, 0F, 0F);
+      leftarm = new ModelRenderer(this, 40, 16);
+      leftarm.addBox(-1F, -2F, -2F, 4, 12, 4);
+      leftarm.setRotationPoint(5F, 2F, 0F);
+      leftarm.setTextureSize(64, 32);
+      leftarm.mirror = true;
+      setRotation(leftarm, 0F, 0F, 0F);
+      rightleg = new ModelRenderer(this, 0, 16);
+      rightleg.addBox(-2F, 0F, -2F, 4, 12, 4);
+      rightleg.setRotationPoint(-2F, 12F, 0F);
+      rightleg.setTextureSize(64, 32);
+      rightleg.mirror = true;
+      setRotation(rightleg, 0F, 0F, 0F);
+      leftleg = new ModelRenderer(this, 0, 16);
+      leftleg.addBox(-2F, 0F, -2F, 4, 12, 4);
+      leftleg.setRotationPoint(2F, 12F, 0F);
+      leftleg.setTextureSize(64, 32);
+      leftleg.mirror = true;
+      setRotation(leftleg, 0F, 0F, 0F);
+      Helmrim1 = new ModelRenderer(this, 0, 0);
+      Helmrim1.addBox(-4F, -13F, -4F, 8, 4, 8);
+      Helmrim1.setRotationPoint(0F, 0F, 0F);
+      Helmrim1.setTextureSize(64, 32);
+      Helmrim1.mirror = true;
+      setRotation(Helmrim1, 0F, 0F, 0F);
+      Shape1 = new ModelRenderer(this, 0, 0);
+      Shape1.addBox(-3F, -12F, 4F, 1, 4, 1);
+      Shape1.setRotationPoint(0F, 0F, 0F);
+      Shape1.setTextureSize(64, 64);
+      Shape1.mirror = true;
+      setRotation(Shape1, 0F, 0F, 0F);
+      Shape2 = new ModelRenderer(this, 0, 0);
+      Shape2.addBox(-1F, -12F, 4F, 2, 4, 1);
+      Shape2.setRotationPoint(0F, 0F, 0F);
+      Shape2.setTextureSize(64, 64);
+      Shape2.mirror = true;
+      setRotation(Shape2, 0F, 0F, 0F);
+      Shape3 = new ModelRenderer(this, 0, 0);
+      Shape3.addBox(2F, -12F, 4F, 1, 4, 1);
+      Shape3.setRotationPoint(0F, 0F, 0F);
+      Shape3.setTextureSize(64, 64);
+      Shape3.mirror = true;
+      setRotation(Shape3, 0F, 0F, 0F);
+  }
+  
+  public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5)
+  {
+    super.render(entity, f, f1, f2, f3, f4, f5);
+    setRotationAngles(f, f1, f2, f3, f4, f5, entity);
+    Helmrim.render(f5);
+    head.render(f5);
+    body.render(f5);
+    rightarm.render(f5);
+    leftarm.render(f5);
+    rightleg.render(f5);
+    leftleg.render(f5);
+    Helmrim.render(f5);
+    Shape1.render(f5);
+    Shape1.render(f5);
+    Shape1.render(f5);
+  }
+  
+  private void setRotation(ModelRenderer model, float x, float y, float z)
+  {
+    model.rotateAngleX = x;
+    model.rotateAngleY = y;
+    model.rotateAngleZ = z;
+  }
+  
+  public void setRotationAngles(float f, float f1, float f2, float f3, float f4, float f5, Entity entity)
+  {
+    super.setRotationAngles(f, f1, f2, f3, f4, f5, entity);
+  }
+
+}
